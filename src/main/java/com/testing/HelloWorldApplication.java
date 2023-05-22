61f9797acd6ea7347be8961b4cf7d27b5c2fc117
rajusfdevops <133023821+rajusfdevops@users.noreply.github.com>
2023-05-22 21:01:11 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/testing/HelloWorldApplication.java
+++ src/main/java/com/testing/HelloWorldApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = { "com.testing" })
 public class HelloWorldApplication {
-    
+    // to trigger genkin build
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
